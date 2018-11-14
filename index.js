1e4ac9906f8c908915568139a182059b46853899
Jangala Latha <janlatha@publicisgroupe.net>
2018-11-14 18:51:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import './client/sass/styles.scss';
+
+console.log("hello i am from index.js.....");
+
+window.onload = codeAddress;
+
+function codeAddress(){
+    var
+    carousel = document.querySelector('.carousel'),
+    figure = carousel.querySelector('figure'),
+    nav = carousel.querySelector('nav'),
+    numImages = figure.childElementCount,
+    theta =  2 * Math.PI / numImages,
+    currImage = 0
+;
+    
+nav.addEventListener('click', onClick, true);
+
+function onClick(e) {
+    e.stopPropagation();
+    
+    var t = e.target;
+    if (t.tagName.toUpperCase() != 'BUTTON')
+        return;
+    
+    if (t.classList.contains('next')) {
+        currImage++;
+    }
+    else {
+        currImage--;
+    }
+    
+    figure.style.transform = `rotateY(${currImage * -theta}rad)`;
+}
+}
+
